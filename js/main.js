a62ded192849988ef8397f341fd344e99c7492a6
vloadka <90221568+vloadka@users.noreply.github.com>
2023-03-10 16:24:31 +0300

    Создание функций и связывание файлов

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function randomNumber(min, max) {
+  if (max < 0 || min < 0) {
+    return('Диапазон может быть только положительный, измените значение');
+  }
+  if (max< min){
+    return('min значение не может быть больше max');
+  }
+  if (max === min) {
+    return max
+  }
+  return Math.floor(Math.random()*(max-min+1))+min;
+}
+
+
+function lengthStringСheck (string, maxLength){
+   return string.length <= maxLength;
+}
+
+
