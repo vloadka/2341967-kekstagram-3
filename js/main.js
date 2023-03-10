69996952963bfcc3ac94468f44e0145860b74631
vloadka <90221568+vloadka@users.noreply.github.com>
2023-03-10 16:49:56 +0300

    Исправление

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function randomNumber(min, max) {
   if (max < 0 || min < 0) {
     return('Диапазон может быть только положительный, измените значение');
   }
-  if (max< min){
+  if (min > max){
     return('min значение не может быть больше max');
   }
   if (max === min) {
@@ -11,9 +11,6 @@ function randomNumber(min, max) {
   return Math.floor(Math.random()*(max-min+1))+min;
 }
 
-
 function lengthStringСheck(string, maxLength){
-   return string.length <= maxLength;
+  return string.length <= maxLength;
 }
-
-
