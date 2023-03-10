77a2bf2f9107d192c2e06be553fe0dc2f8ad24da
vloadka <90221568+vloadka@users.noreply.github.com>
2023-03-10 17:10:38 +0300

    вызов функций

--- js/main.js
+++ js/main.js
@@ -10,7 +10,9 @@ function randomNumber(min, max) {
   }
   return Math.floor(Math.random()*(max-min+1))+min;
 }
+randomNumber(0,10);
 
 function lengthStringСheck(string, maxLength){
   return string.length <= maxLength;
 }
+lengthStringСheck('123',4);
